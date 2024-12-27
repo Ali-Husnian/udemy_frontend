370b870d9904ca94dd1a9d2e71762488eb2952ca
ali husnain <husnainmcs.dev@gmail.com>
2024-12-27 22:07:00 +0500

    first commit

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-black font-bold">This page doesn't exist</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
